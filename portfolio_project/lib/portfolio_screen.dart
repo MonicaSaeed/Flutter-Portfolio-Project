dcedac00154357c2bd7d041b40991e01b4d4acdf
Monica Saeed <monicasaeed12@gmail.com>
2025-05-28 14:56:32 +0300

    add text

--- portfolio_project/lib/portfolio_screen.dart
+++ portfolio_project/lib/portfolio_screen.dart
@@ -15,7 +15,38 @@ class PortfolioScreen extends StatelessWidget {
             CircleAvatar(
               radius: 80,
               backgroundImage: AssetImage('assets/images/me.jpg'),
-            )
+              // child: SvgPicture.asset(
+              //   'assets/images/me.svg',
+              //   width: 180,
+              //   height: 180,
+              //   fit: BoxFit.cover,
+              // ),
+            ),
+            SizedBox(
+              height: 34,
+            ),
+            Text(
+              'Hi, I am Monica,\nCreative\nTechnologist',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF21243d)),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text(
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xFF21243d),
+                ),
+              ),
+            ),
           ],
         ),
       ),
