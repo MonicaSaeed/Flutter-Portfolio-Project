caad55ca0ab10ceb725f5a5aac4422d3102b2d5f
Monica Saeed <monicasaeed12@gmail.com>
2025-05-28 13:59:38 +0300

    initial project

---
+++ portfolio_project/lib/portfolio_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PortfolioScreen extends StatelessWidget {
+  const PortfolioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
